4cfec8dabd0354d5f49422182e988839a0d468d8
Danny Camenisch <danny@camenisch.ws>
2020-06-26 18:55:49 +0200

    Initial Commit

---
+++ Shared/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  Shared
+//
+//  Created by Danny on 26.06.20.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
